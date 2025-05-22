c1ddc5675643097337739c750c21ab5dad1dfea5
Tu Nombre <usuario@ejemplo.com>
2025-05-21 22:01:43 -0300

    Actualización de la configuración de TypeScript: se ha cambiado el objetivo a ES5, se han añadido nuevas opciones de compilación y se han modificado las rutas de inclusión y exclusión en tsconfig.json.

---
+++ next.config.ts
@@ -0,0 +1,16 @@
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '2mb'
+    },
+  },
+  images: {
+    domains: [],
+  },
+}
+
+export default nextConfig
---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm">
+        <h1 className="text-4xl font-bold text-center mb-8">
+          Bienvenido al Boilplate de nombre de la startup
+        </h1>
+        <p className="text-center">
+          Este es un boilerplate para crear un microsass.
+        </p>
+      </div>
+    </main>
+  )
+}
